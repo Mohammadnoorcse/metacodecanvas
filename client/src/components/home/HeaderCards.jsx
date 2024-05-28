fee666dd2b1dada06af256dc7bd48100a8c6ad2f
mohammadnoor <mohammadnoorcse@gmail.com>
2024-05-28 16:56:10 +0600

    working home2 page

---
+++ client/src/components/home/HeaderCards.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+
+const HeaderCards = ({value}) => {
+  return (
+    <Link to={value.name} className="w-full">
+       <div className="h-auto flex justify-center items-center rounded-2xl card-item" style={{backgroundColor:`${value.bg}`}}>
+        <img src={value.icon} alt={value.name} className="w-[10rem] h-[10rem]" />
+
+       </div>
+    </Link>
+  )
+}
+
+export default HeaderCards
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,9 +1,11 @@
 import Home1 from "../components/home/Home1"
+import Home2 from "../components/home/Home2"
 
 const Home = () => {
   return (
    <>
     <Home1/>
+    <Home2/>
    
    </>
   )
