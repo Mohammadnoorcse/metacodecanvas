803800f1d489884bc5bcb9211afcc283870b912a
mohammadnoor <mohammadnoorcse@gmail.com>
2024-07-15 14:01:33 +0600

    working nothing

--- client/src/components/quiz/Quiz.jsx
+++ client/src/components/quiz/Quiz.jsx
@@ -1,30 +1,7 @@
 
 
 
-//  const questions = [
-//     {   
-//         // headerName
-//         questionText: 'What is the capital of France?',
-//         answerOptions: [
-//             { answerText: 'New York', isCorrect: false },
-//             { answerText: 'London', isCorrect: false },
-//             { answerText: 'Paris', isCorrect: true },
-//             { answerText: 'Dublin', isCorrect: false },
-//         ],
-//     },
-//     {  
-//         // headerName
-//         // code
-//         questionText: 'Who is CEO of Tesla?',
-//         answerOptions: [
-//             { answerText: 'Jeff Bezos', isCorrect: false },
-//             { answerText: 'Elon Musk', isCorrect: true },
-//             { answerText: 'Bill Gates', isCorrect: false },
-//             { answerText: 'Tony Stark', isCorrect: false },
-//         ],
-//     },
-    
-// ];
+
 
 
 
